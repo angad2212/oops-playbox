77fd50f64fc752e82380b0c3b428f05d04d24ae0
angad2212 <angadsuryavanshi22@gmail.com>
2025-10-06 15:47:26 +0530

    added second assigment based on inheritance

---
+++ 2-vehicle.cpp
@@ -0,0 +1,58 @@
+/*
+Problem:Create a `Vehicle` base class and derived classes `Car`, `Bike`, and `Truck`.
+
+Each derived class should override a `displayInfo()` function and add its own attributes.
+
+Concepts Tested: Inheritance, overriding, base constructor call
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Vehicle{
+    public:
+    Vehicle(){
+        cout << "Hey there, i am a vehicle" << endl;
+    }
+    void displayInfo(){
+        cout << "Vehicle" << endl;
+    };
+};
+
+class Car : public Vehicle{
+    public:
+    Car(){
+        cout << "Hey there, i am a car" << endl;
+    }
+    void displayInfo(){
+        cout << "Car" << endl;
+    }
+};
+
+class Bike : public Vehicle{
+    public:
+    void displayInfo(){
+        cout << "Bike" << endl;
+    }
+};
+
+class Truck : public Vehicle{
+    public:
+    void displayinfo(){
+        cout << "Truck" << endl;
+    }
+};
+
+int main(){
+    Car car;
+    Bike bike;
+    Truck truck;
+    car.displayInfo();
+    bike.displayInfo();
+    truck.displayInfo();
+    return 0;
+}
