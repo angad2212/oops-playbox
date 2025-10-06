6418a5cf37b94fd2729f41d968163967a6c6183f
angad2212 <angadsuryavanshi22@gmail.com>
2025-10-06 15:36:35 +0530

    added first assigment

---
+++ 1-bank_account.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class BankAccount {
+private:
+    int account_number;
+    int balance;
+    string owner_name;
+public: 
+    BankAccount(int account_number, int balance, string owner_name){
+        this->account_number = account_number;
+        this->balance = balance;
+        this->owner_name = owner_name;
+    }
+    void deposit(int amount){
+        balance += amount;
+    }
+    void withdraw(int amount){
+        balance -= amount;
+    }
+    void get_balance(int account_number){
+        cout << "Account Number: " << account_number << endl;
+        cout << "Balance: " << balance << endl;
+        cout << "Owner Name: " << owner_name << endl;
+    }
+};
+
+int main(){
+    BankAccount acc(12345, 1000, "Angad");
+    acc.deposit(200);
+    acc.withdraw(50);
+    acc.get_balance(12345);
+    return 0;
+}
